d91fc39222f788f1fc326ead5263df8a03bcdf4c
Mounika <aretemounika@gmail.com>
2023-01-23 18:26:45 -0600

    completed the code

--- src/com/assignment/task3/Program7_star1.java
+++ src/com/assignment/task3/Program7_star1.java
@@ -1,4 +1,17 @@
 package com.assignment.task3;
 
 public class Program7_star1 {
+    public static void main(String[] args) {
+        for(int i =1; i<=5;i++){
+            for(int j=5;j>=1;j--){
+                if(i>=j){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
 }
